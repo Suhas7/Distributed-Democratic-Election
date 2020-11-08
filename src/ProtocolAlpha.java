be78a1f3c9b5bc868c4c342a794667f3d2aedd10
Kaylee Trevino <kayleetrevino@Kaylees-MBP.lan>
2020-11-08 15:38:01 -0600

    Refactored code for reusability

--- src/ProtocolAlpha.java
+++ src/ProtocolAlpha.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class ProtocolAlpha {
     //TODO CONCERN: ONLY RELIES ON FIRST CHOICES
     public static String runProtocol(Voter[] voters, int numFaultyVoters) {
-        String[] methods = new String[] {"exchangeFirstVote", "setPerplexed", "sendPerplexed", "setAlert"};
+        String[] methods = new String[] {"proposeTopChoice", "setPerplexed", "sendPerplexed", "setAlert"};
 
         for (int i = 0; i < methods.length; i++) {
             ArrayList<Thread> allThreads = new ArrayList<>();
@@ -83,71 +83,6 @@ public class ProtocolAlpha {
                 }
             }
             return currMaxString;
-        }
-    }
-
-    static class Voter {
-        private final String[] myVotes;
-        private String[] allVotes;
-        private boolean[] perplexedVoters;
-        private boolean alert;
-        private Object lock;
-
-        public Voter(String[] votes, int numVoters) {
-            this.myVotes = votes;
-            this.allVotes = new String[numVoters];
-            this.perplexedVoters = new boolean[numVoters];
-            this.alert = false;
-            //this.lock = new Object();
-        }
-
-        public void exchangeFirstVote(Voter[] voters, Integer myIdx) {
-            for (Voter voter : voters) {
-                voter.receiveVote(myVotes[0], myIdx); //sending my vote
-            }
-        }
-
-        public void receiveVote(String vote, int i) {
-            allVotes[i] = vote;
-        }
-
-        public void setPerplexed(Integer numFaultyVoters, Integer myIdx) {
-            int diff = 0;
-            for (String vote : this.allVotes) {
-                if (!vote.equals(allVotes[myIdx])) diff++;
-            }
-
-            if (diff >= (allVotes.length - numFaultyVoters)/2.0) {
-                perplexedVoters[myIdx] = true;
-            }
-        }
-
-        public void sendPerplexed(Voter[] voters, Integer myIdx) {
-            if (perplexedVoters[myIdx]) {
-                for (Voter voter : voters) {
-                    voter.receivePerplexedMessage(myIdx); //sending "I am perplexed" message
-                }
-            }
-        }
-
-        public void receivePerplexedMessage(int i) {
-            perplexedVoters[i] = true;
-        }
-
-        public void setAlert(Integer numFaultyVoters) {
-            int numPerplexed = 0;
-            for (boolean isPerplexed : perplexedVoters) {
-                if (isPerplexed) numPerplexed++;
-            }
-            if (numPerplexed >= allVotes.length - 2 * numFaultyVoters) this.alert = true;
-        }
-
-        public boolean getAlert() {
-            return this.alert;
-        }
-
-        public String getVote() {
-            return this.myVotes[0];
         }
     }
 }
--- src/ProtocolBeta.java
+++ src/ProtocolBeta.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class ProtocolBeta {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class ProtocolBeta {
+    public static String runProtocol(Voter[] voters, int numFaultyVoters) {
+        String[] methods = new String[] {"proposeTopChoice", "setPerplexed", "sendPerplexed", "setAlert"};
+
+        for (int i = 0; i < methods.length; i++) {
+            ArrayList<Thread> allThreads = new ArrayList<>();
+            final int finalI = i;
+            for (int j = 0; j < voters.length; j++) {
+                final int finalJ = j;
+                Object[][] params = new Object[][] {new Object[] {voters, finalJ}, new Object[] {numFaultyVoters, finalJ}, new Object[] {voters, finalJ}, new Object[] {numFaultyVoters}};
+                Thread t = new Thread(() -> {
+                    try {
+                        Class[] classes = new Class[params[finalI].length];
+                        for (int k = 0; k < params[finalI].length; k++) {
+                            classes[k] = params[finalI][k].getClass();
+                        }
+
+                        Method currMethod = voters[finalJ].getClass().getMethod(methods[finalI], classes);
+                        currMethod.invoke(voters[finalJ], params[finalI]);
+                    } catch (NoSuchMethodException | SecurityException | IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+                        throw new Error("Error: " + e + " on method: " + methods[finalI] + " because: " + e.getCause() + ", " + e.getLocalizedMessage());
+                    }
+                });
+                allThreads.add(t);
+            }
+
+            for (Thread t : allThreads) {
+                t.start();
+            }
+
+            for (Thread t : allThreads) {
+                try {
+                    t.join();
+                } catch (InterruptedException e) {
+                    throw new Error("Interrupted exception in threads!");
+                }
+            }
+        }
+        return "";
+    }
 }
